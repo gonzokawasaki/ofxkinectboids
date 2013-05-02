97cac613f4fa9f30b6028fda7b881a3fd302188d
gonzokawasaki <kieronodonnell@yahoo.co.uk>
2013-05-02 17:33:31 +0100

    added voids code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include "testApp.h"
 #include "ofMain.h"
 #include "ofAppGlutWindow.h"
@@ -15,3 +15,23 @@ int main( ){
 	ofRunApp( new testApp());
 
 }
+
+*/
+
+ #include "ofMain.h"
+ #include "testApp.h"
+ #include "ofAppGlutWindow.h"
+ 
+ //========================================================================
+ int main( ){
+ 
+ ofAppGlutWindow window;
+ ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+ 
+ // this kicks off the running of my app
+ // can be OF_WINDOW or OF_FULLSCREEN
+ // pass in width and height too:
+ ofRunApp( new testApp());
+ 
+ }
+
